fd24ce965819b5d8c2bafaae754422ffde8e21d6
er-ssmohanty <pie2001@protonmail.com>
2021-11-26 22:52:01 +0530

    sorting in progress-4

--- sorting.asm
+++ sorting.asm
@@ -4,17 +4,19 @@
 ;;C register acts as a counter. 
 ;;HL register pair has the current value. 
 ;;A(accumulator) register holds the maximum value.
+
+;------INITIALISATION------;
 start:  mov b, 10    
 	mov c, b     
 	lxi d, 3000H  
 	mov h,d
 	mov l,e	     
 	mov a,m	     
+;--------------------------;
 	
-
+;----MAX VALUE FINDER------;
 max:    inx h
 	cmp m
-
 	jnc skip
 	mov a,m
 skip:	dcr c
