077cd00d63054b8676f5088bd48e362d977034ac
er-ssmohanty <pie2001@protonmail.com>
2021-11-26 22:42:37 +0530

    sorting in progress-1

--- sorting.asm
+++ sorting.asm
@@ -1,6 +1,20 @@
-start:  mov c, 10
+start:  mov b, 10
+	mov c, b
 	lxi d, 3000H;de pair has the largest value 
 	mov h,d
 	mov l,e	    ;hl pair has the current value
 	mov a,m
 	
+
+max:   inx h
+	cmp m
+
+	jnc skip
+	mov a,m
+skip:	dcr c
+
+	jnz max
+	
+	inx h
+	mov m,a
+	hlt
