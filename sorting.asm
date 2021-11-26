639cd80d99bfe00fd80b8b965eeb4d17e9d2d6f7
er-ssmohanty <pie2001@protonmail.com>
2021-11-26 22:43:54 +0530

    sorting in progress-2

--- sorting.asm
+++ sorting.asm
@@ -1,12 +1,12 @@
-start:  mov b, 10
-	mov c, b
+start:  mov b, 10	;b reg holds block size
+	mov c, b	;c reg is the counter
 	lxi d, 3000H;de pair has the largest value 
 	mov h,d
 	mov l,e	    ;hl pair has the current value
 	mov a,m
 	
 
-max:   inx h
+max:    inx h
 	cmp m
 
 	jnc skip
