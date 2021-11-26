0864034075645802b3e9fc6c78fded570ab5385c
er-ssmohanty <pie2001@protonmail.com>
2021-11-26 23:20:58 +0530

    sorting in progress-6

--- sorting.asm
+++ sorting.asm
@@ -1,9 +1,20 @@
+;------------PREFACE-----------------;
+; AUTHOR: Subhransu S Mohanty, VSSUT Burla(Odisha)
+; This program was writtern to implement selection sort
+; algorithm in 8085 assembly language. This asm file
+; was created on 26th November 2021.
+
+
+
+;--------DESCRIPTION---------;
+
 ;;The topmost two locations of the stack 
 ;have the address of the largest value. 
 ;;B register holds the block size. 
 ;;C register acts as a counter. 
 ;;HL register pair has the current value. 
 ;;A(accumulator) register holds the maximum value.
+;;D register acts as the temporary variable.
 
 ;------INITIALISATION------;
 start:  mov b, 10    
@@ -25,7 +36,13 @@ skip:	dcr c
 ;--------------------------;
 
 	cmp m	
-
+	; if max value is greater than current 
+	;carry and zero are reset
+	jc skip2
+	mov d,m
+	mov m,a
+	mov a,d
+skip2:
 	inx h
 	mov m,a
 	hlt
