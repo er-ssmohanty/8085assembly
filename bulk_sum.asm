f7e46b1a8ce714e956d48e112a36b5f4bc99d9d5
er-ssmohanty <pie2001@protonmail.com>
2021-11-27 21:33:46 +0530

    added block summer program

---
+++ bulk_sum.asm
@@ -0,0 +1,16 @@
+start:  lxi h, 3000H ;starting location of the block
+	mvi c, 5 ; block size
+	mvi b, 0
+
+loop:   add m
+	jnc skip
+	inr b	
+skip:	inx h
+	dcr c
+	jnz loop
+	mov m,a
+	inx h
+	mov m,b
+	hlt
+	;rlc
+	
