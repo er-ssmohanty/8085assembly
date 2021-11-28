381adab1276dbca45c2de7b15fb416cd1bc4f1e7
er-ssmohanty <pie2001@protonmail.com>
2021-11-28 11:43:54 +0530

    added reference to positive spike generation in negative spike program

--- negative-spike-signal.asm
+++ negative-spike-signal.asm
@@ -7,7 +7,8 @@ loop:   mvi a,0C0H
 
 ;delay after logic high and no delay after logic low 
 ;helps create negative spikes
-
+;for positive spike signal generation remove delay
+;after logic high and place delay after logic low
 delay:  mvi c,78H	;7T states
 loop2:	dcr c	 	;4T states
 	jnz loop2	;10T states, last one 7t
